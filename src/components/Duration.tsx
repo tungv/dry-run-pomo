420c3fb1e93ca08a9a32d66d3da401f2d739ba46
Tung Vu <me@tungv.com>
2021-10-29 02:31:54 +0700

    chore: add styling
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/components/Duration.tsx
+++ src/components/Duration.tsx
@@ -1,5 +1,7 @@
 import { CSSProperties } from "react";
 
+// https://developer.mozilla.org/en-US/docs/Web/CSS/font-variant-numeric
+
 export default function Duration(props) {
   const { minutes, seconds, milliseconds } = duration(
     props.futureMs,
@@ -7,8 +9,25 @@ export default function Duration(props) {
   );
   return (
     <div>
-      <span>{minutes}</span>:<span>{seconds}</span>
-      <span>.{milliseconds}</span>
+      <span className="minute">{minutes}</span>:
+      <span className="second">{seconds}</span>.
+      <span className="millisecond">{milliseconds.substr(0, 3)}</span>
+      <style jsx>{`
+        span {
+          font-variant-numeric: tabular-nums;
+        }
+        .minute {
+          font-size: 1.5rem;
+        }
+
+        .second {
+          font-size: 1.5rem;
+        }
+
+        .millisecond {
+          font-size: 0.5rem;
+        }
+      `}</style>
     </div>
   );
 }
@@ -24,7 +43,10 @@ function duration(
   const eta = future - start;
 
   const seconds = String(Math.floor((eta / 1000) % 60)).padStart(2, "0");
-  const minutes = String(Math.floor((eta / 1000 / 60) % 60));
+  const minutes = String(Math.floor((eta / 1000 / 60) % 60)).padStart(
+    2,
+    "\xa0",
+  );
   const milliseconds = String(eta % 1000).padStart(3, "0");
   return { minutes, seconds, milliseconds };
 }
