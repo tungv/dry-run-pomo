7972503b0f0bc0c10b5ad271c2a9769064fbcabc
Tung Vu <me@tungv.com>
2021-10-30 01:37:22 +0700

    refactor: usePomodoroState
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import CountDown from "../components/CountDown";
 import Duration from "../components/Duration";
 import getDuration from "../components/getDuration";
 
-const FULL_CYCLE = 25 * 60 * 1000;
-// const FULL_CYCLE = 0.05 * 60 * 1000;
+// const FULL_CYCLE = 25 * 60 * 1000;
+const FULL_CYCLE = 0.05 * 60 * 1000;
 
 export default function PomodoroHome() {
-  const [stopTime, setStopTime] = useState(0);
-  const [pauseTime, setPausing] = useState(0);
+  const { stopTime, pauseTime, start, pause, reset, resume } =
+    usePomodoroState();
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -17,14 +17,14 @@ export default function PomodoroHome() {
 
       document.title = `${minutes}:${seconds}`;
 
+      // do nothing when paused
       if (pauseTime) {
         return;
       }
-      if (stopTime < Date.now()) {
-        setStopTime(0);
-      }
 
-      if (stopTime === 0) {
+      // reset when time is up
+      if (stopTime < Date.now()) {
+        reset();
         clearInterval(interval);
       }
     }, 100);
@@ -34,28 +34,9 @@ export default function PomodoroHome() {
 
   return (
     <div>
-      <button
-        onClick={() => {
-          setStopTime(Date.now() + FULL_CYCLE);
-          setPausing(0);
-        }}
-      >
-        Start
-      </button>
-      {pauseTime === 0 && (
-        <button onClick={() => setPausing(Date.now())}>pause</button>
-      )}
-      {pauseTime !== 0 && (
-        <button
-          onClick={() => {
-            const timeLeft = stopTime - pauseTime;
-            setPausing(0);
-            setStopTime(Date.now() + timeLeft);
-          }}
-        >
-          resume
-        </button>
-      )}
+      <button onClick={start}>Start</button>
+      {pauseTime === 0 && <button onClick={pause}>pause</button>}
+      {pauseTime !== 0 && <button onClick={resume}>resume</button>}
 
       {stopTime === 0 ? (
         <Duration futureMs={FULL_CYCLE} startMs={0} />
@@ -64,8 +45,59 @@ export default function PomodoroHome() {
       ) : (
         <CountDown to={stopTime} />
       )}
-
-      <pre>{JSON.stringify({ stopTime, pauseTime }, null, 2)}</pre>
     </div>
   );
+}
+
+interface PomodoroState {
+  stopTime: number;
+  pauseTime: number;
+}
+
+type PomodoroAction =
+  | { type: "start" }
+  | { type: "pause" }
+  | { type: "reset" }
+  | { type: "resume" };
+
+const initialState: PomodoroState = {
+  stopTime: 0,
+  pauseTime: 0,
+};
+
+function reducer(state: PomodoroState, action: PomodoroAction): PomodoroState {
+  switch (action.type) {
+    case "start":
+      return {
+        ...state,
+        stopTime: Date.now() + FULL_CYCLE,
+        pauseTime: 0,
+      };
+    case "pause":
+      return {
+        ...state,
+        pauseTime: Date.now(),
+      };
+    case "reset":
+      return initialState;
+
+    case "resume":
+      return {
+        ...state,
+        stopTime: state.stopTime + (Date.now() - state.pauseTime),
+        pauseTime: 0,
+      };
+  }
+}
+
+function usePomodoroState() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return {
+    ...state,
+    start: () => dispatch({ type: "start" }),
+    pause: () => dispatch({ type: "pause" }),
+    reset: () => dispatch({ type: "reset" }),
+    resume: () => dispatch({ type: "resume" }),
+  };
 }
